697f1d4262a8f38f975dad1087f691c2b4a23a79
Mikaela Peters <petermik@oregonstate.edu>
2019-11-18 15:21:19 -0800

    Data Visualization Milestone 1

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-alert("Hey you!")
+// alert("Hey you!")
 
-$(updateview)
+// $(updateview)
 
-function updateview() {
-    alert("Take a moment to smile! :)")
+// function updateview() {
+//     alert("Take a moment to smile! :)")
+// }
+
+const BASE_URL = "https://zagster-service.herokuapp.com"
+
+$(updateView)
+
+function updateView() {
+  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+}
+
+function updateRideCount(data) {
+  numberOfRides = data.count
+  $("h2#rideCount").html(numberOfRides)
 }
